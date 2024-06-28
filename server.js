78a91251fbc6449c5d5bfa2669df7ef20543319a
Owale128 <Christian.buaka@hotmail.com>
2024-06-28 23:17:38 +0200

    I put the files in separate folders and also connected to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
-const User = require('./userModel')
+const User = require('./model/userModel')
 const bcrypt = require('bcryptjs');
-const auth = require('./auth')
+const connectDB = require('./database/db')
+const auth = require('./middleware/auth')
 const app = express();
 
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyparser.json());
 app.use(cors());
 
+connectDB()
 
 app.get('/', auth, async (req, res) => {
     try {
